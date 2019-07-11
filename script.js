e33c025f9a80bc0a7897a002d60907009454c99c
arifa-chouhdry <48832658+arifa-chouhdry@users.noreply.github.com>
2019-07-11 12:17:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// var jsonString = '[{"name":"John","score":51},{"name":"Jack","score":17}]';
+var contact = '{ "contacts" : [' +
+'{ "Name":"Sofia" , "number":"0333-2158336" ,"chat": "hi" , "lastSeen" : "2:25 AM"},' +
+'{ "Name":"Arifa" , "number":"0303-2158336"  ,"chat": "hi" , "lastSeen" : "3:25 AM"  },' +
+'{ "Name":"Ali" , "number":"0300-2158336" ,"chat": "hi"  , "lastSeen" : "2:25 PM"} ]}' +
+'{ "Name":"Sana" , "number":"0301-2158336" , "chat": "hi" , "lastSeen" : "4:25 PM"} ]}' +
+'{ "Name":"Ambreen" , "number":"0301-2158336" , "chat": "hi" , "lastSeen" : "5:25 PM"} ]}';
